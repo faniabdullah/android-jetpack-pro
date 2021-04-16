fd2b055175654b223fb0f3549a3ac1fa039ab7f3
faniabdullah <faniabdullah27@gmail.com>
2021-04-17 01:41:02 +0700

    set submission to lokal

--- app/src/main/java/com/bangkit/faniabdullah_jetpack/model/MovieEntity.kt
+++ app/src/main/java/com/bangkit/faniabdullah_jetpack/model/MovieEntity.kt
@@ -7,12 +7,10 @@ import kotlinx.parcelize.Parcelize
 data class MovieEntity(
     val id: Int,
     val backdrop_path: String,
-    val original_language: String,
     val original_title: String,
     val overview: String,
     val poster_path: String,
     val release_date: String,
-    val title: String,
     val vote_average: Double,
     val vote_count: Int
 ) : Parcelable
