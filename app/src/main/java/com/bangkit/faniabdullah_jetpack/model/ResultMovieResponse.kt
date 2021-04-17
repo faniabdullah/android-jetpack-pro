ac9b4459788c8731c4d56299b5862ae411e8723a
faniabdullah <faniabdullah27@gmail.com>
2021-04-17 17:33:57 +0700

    fix submission 1 is used local data

--- app/src/main/java/com/bangkit/faniabdullah_jetpack/model/ResultMovieResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.bangkit.faniabdullah_jetpack.model
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class ResultMovieResponse(
-    val results: ArrayList<MovieEntity>
-):Parcelable
