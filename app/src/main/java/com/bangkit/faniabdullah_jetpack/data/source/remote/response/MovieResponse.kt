b95fef017ca45b8ab12a454a8098c5e429c3a44b
faniabdullah <faniabdullah27@gmail.com>
2021-04-27 01:35:21 +0700

    structure component

--- app/src/main/java/com/bangkit/faniabdullah_jetpack/data/source/remote/response/MovieResponse.kt
+++ app/src/main/java/com/bangkit/faniabdullah_jetpack/data/source/remote/response/MovieResponse.kt
@@ -1,11 +1,8 @@
 package com.bangkit.faniabdullah_jetpack.data.source.remote.response
 
-import android.os.Parcelable
 import com.bangkit.faniabdullah_jetpack.data.source.local.MovieEntity
 import com.google.gson.annotations.SerializedName
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class MovieResponse<T>(
     @SerializedName("status_message")
     val statusMessage: String? = null,
@@ -13,4 +10,4 @@ data class MovieResponse<T>(
     val statusCode: Int? = null,
     @SerializedName("results")
     val result: List<MovieEntity>? = null
-) : Parcelable
+)
