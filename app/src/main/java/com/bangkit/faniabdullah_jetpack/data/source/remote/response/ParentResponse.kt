a4430cedb993feadbe9bead090901eb0d3116caa
faniabdullah <faniabdullah27@gmail.com>
2021-04-27 05:07:17 +0700

    add parent response

---
+++ app/src/main/java/com/bangkit/faniabdullah_jetpack/data/source/remote/response/ParentResponse.kt
@@ -0,0 +1,19 @@
+package com.bangkit.faniabdullah_jetpack.data.source.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ParentResponse<T>(
+
+	@field:SerializedName("page")
+	val page: Int? = null,
+
+	@field:SerializedName("total_pages")
+	val totalPages: Int? = null,
+
+	@field:SerializedName("results")
+	val results: List<T?>? = null,
+
+	@field:SerializedName("total_results")
+	val totalResults: Int? = null
+)
+
