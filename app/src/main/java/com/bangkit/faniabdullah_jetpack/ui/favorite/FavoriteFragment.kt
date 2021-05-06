9c99affeec79317a598f5db04f9559e316d29ccb
faniabdullah <faniabdullah27@gmail.com>
2021-05-06 19:54:00 +0700

    paging in favorite movie

--- app/src/main/java/com/bangkit/faniabdullah_jetpack/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/bangkit/faniabdullah_jetpack/ui/favorite/FavoriteFragment.kt
@@ -1,7 +1,7 @@
 package com.bangkit.faniabdullah_jetpack.ui.favorite
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,11 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.bangkit.faniabdullah_jetpack.data.source.local.entity.MovieEntity
-import com.bangkit.faniabdullah_jetpack.data.source.local.entity.TvShowsEntity
 import com.bangkit.faniabdullah_jetpack.databinding.FragmentFavoriteBinding
-import com.bangkit.faniabdullah_jetpack.ui.detailmovie.DetailActivity
-import com.bangkit.faniabdullah_jetpack.utils.Constant
 import com.bangkit.faniabdullah_jetpack.utils.ViewModelFactory
 
 class FavoriteFragment : Fragment() {
@@ -39,46 +35,39 @@ class FavoriteFragment : Fragment() {
         movieAdapter = MovieFavoriteAdapter()
         tvShowsAdapter = TvShowsFavoriteAdapter()
 
-        binding.favoriteMovie.apply {
-            rvMovie.layoutManager =
-                LinearLayoutManager(activity, GridLayoutManager.HORIZONTAL, false)
-            rvMovie.setHasFixedSize(true)
-            rvMovie.adapter = movieAdapter
-        }
 
-        binding.favoriteTvshows.apply {
-            rvTvShows.layoutManager =
-                LinearLayoutManager(activity, GridLayoutManager.HORIZONTAL, false)
-            rvTvShows.setHasFixedSize(true)
-            rvTvShows.adapter = tvShowsAdapter
-        }
-
-        val factory = ViewModelFactory.getInstance(requireActivity())
-        favoriteViewModel = ViewModelProvider(this, factory)[FavoriteViewModel::class.java]
+        if (activity != null) {
+            val factory = ViewModelFactory.getInstance(requireActivity())
+            favoriteViewModel = ViewModelProvider(this, factory)[FavoriteViewModel::class.java]
 
 
-        favoriteViewModel.getFavoriteMovie().observe(viewLifecycleOwner, { movie ->
-            movieAdapter.submitList(movie)
-        })
+            favoriteViewModel.getFavoriteMovie().observe(viewLifecycleOwner, { movie ->
 
-        favoriteViewModel.getFavoriteTvShows().observe(viewLifecycleOwner, { movie ->
-            tvShowsAdapter.submitList(movie)
-        })
-    }
+                movieAdapter.submitList(movie)
+                movieAdapter.notifyDataSetChanged()
+            })
 
-    private fun showDetailMovie(movie: MovieEntity?, tvShows: TvShowsEntity?) {
-        val intentDetail = Intent(activity, DetailActivity::class.java)
+            favoriteViewModel.getFavoriteTvShows().observe(viewLifecycleOwner, { movie ->
 
-        if (movie != null) {
-            intentDetail.putExtra(Constant.MOVIE_ID, movie.movie_id)
-                .putExtra(Constant.KEY_TYPE, Constant.MOVIE_TYPE)
-        } else {
-            if (tvShows != null) {
-                intentDetail.putExtra(Constant.MOVIE_ID, tvShows.tvShows_id)
-                    .putExtra(Constant.KEY_TYPE, Constant.TV_SHOWS_TYPE)
+                tvShowsAdapter.submitList(movie)
+                movieAdapter.notifyDataSetChanged()
+            })
+
+            binding.favoriteMovie.apply {
+                rvMovie.layoutManager =
+                    LinearLayoutManager(activity, GridLayoutManager.HORIZONTAL, false)
+                rvMovie.setHasFixedSize(true)
+                rvMovie.adapter = movieAdapter
             }
+
+            binding.favoriteTvshows.apply {
+                rvTvShows.layoutManager =
+                    LinearLayoutManager(activity, GridLayoutManager.HORIZONTAL, false)
+                rvTvShows.setHasFixedSize(true)
+                rvTvShows.adapter = tvShowsAdapter
+            }
+
         }
 
-        startActivity(intentDetail)
     }
 }
