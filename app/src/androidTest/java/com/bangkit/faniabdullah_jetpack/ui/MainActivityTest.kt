11f414d11ee3a53e64ce3b12ac0e007a9833b7ae
faniabdullah <faniabdullah27@gmail.com>
2021-05-08 02:25:49 +0700

    is Ok to create test with data empity

--- app/src/androidTest/java/com/bangkit/faniabdullah_jetpack/ui/MainActivityTest.kt
+++ app/src/androidTest/java/com/bangkit/faniabdullah_jetpack/ui/MainActivityTest.kt
@@ -1,9 +1,6 @@
 package com.bangkit.faniabdullah_jetpack.ui
 
-import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.IdlingRegistry
@@ -15,19 +12,16 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.bangkit.faniabdullah_jetpack.R
-import com.bangkit.faniabdullah_jetpack.data.source.local.room.MovieDao
-import com.bangkit.faniabdullah_jetpack.data.source.local.room.MovieDatabase
 import com.bangkit.faniabdullah_jetpack.utils.EspressoIdlingResource
-import org.junit.*
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runners.MethodSorters
-import java.io.IOException
 
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest {
-    private lateinit var db: MovieDatabase
-    private lateinit var movieDao: MovieDao
 
     @get:Rule
     var activityRule = ActivityScenarioRule(MainActivity::class.java)
@@ -35,17 +29,13 @@ class MainActivityTest {
     @Before
     fun setUp() {
         IdlingRegistry.getInstance().register(EspressoIdlingResource.espressoTestIdlingResource)
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        db = Room.inMemoryDatabaseBuilder(
-            context, MovieDatabase::class.java).build()
-        movieDao = db.movieDao()
+
     }
 
     @After
-    @Throws(IOException::class)
     fun tearDown() {
         IdlingRegistry.getInstance().unregister(EspressoIdlingResource.espressoTestIdlingResource)
-        db.close()
+
     }
 
     @Test
@@ -129,10 +119,11 @@ class MainActivityTest {
     @Test
     @Throws(Exception::class)
     fun ShowEmptyFavorite() {
-        movieDao.deleteAllMovie()
-        movieDao.deleteAllTvShows()
         onView(withId(R.id.navigation_favorite)).perform(click())
-
+        onView(withId(R.id.picture_notify_movie))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.picture_notify_tv))
+            .check(matches(isDisplayed()))
     }
 
     @Test
