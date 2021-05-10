4a51df32ca6082ec758b3b79763e668b2c777c9b
faniabdullah <faniabdullah27@gmail.com>
2021-05-10 13:41:59 +0700

    clean up code

--- app/src/main/java/com/bangkit/faniabdullah_jetpack/data/source/local/room/MovieDatabase.kt
+++ app/src/main/java/com/bangkit/faniabdullah_jetpack/data/source/local/room/MovieDatabase.kt
@@ -1,8 +1,6 @@
 package com.bangkit.faniabdullah_jetpack.data.source.local.room
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.bangkit.faniabdullah_jetpack.data.source.local.entity.MovieEntity
 import com.bangkit.faniabdullah_jetpack.data.source.local.entity.TvShowsEntity
